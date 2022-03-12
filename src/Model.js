78e023dc143a29ecaa8b7cf3b94155e3f30fc2a6
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-03-12 15:19:21 +0100

    next

--- src/Model.js
+++ src/Model.js
@@ -6,7 +6,7 @@ export function Scene() {
   const gltf = useLoader(GLTFLoader, '/kruszarka_szczekowa1_wer15_scena3.gltf')
   return (
     <>
-      <primitive object={gltf.scene} />
+      <primitive object={gltf.scene} scale={[0.8,0.8,0.8]} position={[0,0,0]}  />
     </>
   )
 }
